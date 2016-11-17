b323f0ecbc7881d995e10573a1303c85054d1f75
Jason Zhou <jaszhou@ebay.com>
2016-11-17 13:48:30 +0100

    Revert "Read parameters from application.conf"
    This reverts commit 92ec983ff5fb54412120e5effb2a079386372ccb.

--- app/controllers/MarktplaatsUnoAdController.java
+++ app/controllers/MarktplaatsUnoAdController.java
@@ -3,7 +3,6 @@ package controllers;
 import play.Configuration;
 import play.Play;
 import play.libs.ws.WSClient;
-import play.libs.ws.WSRequest;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.uno;
@@ -14,25 +13,21 @@ public class MarktplaatsUnoAdController extends Controller {
 
     @Inject static WSClient ws;
     private static final String myAdsUrl = "https://api.marktplaats.nl/api3/ads/me.json";
-    private static Configuration conf = Play.application().configuration();
-    private static String apiVer = conf.getString("api_ver");
-    private static String accessToken = conf.getString("access_token");
-    private static String session = conf.getString("session");
-    private static String screenWidth = conf.getString("screenWidth");
-    private static String screenHeight = conf.getString("screenHeight");
-    private static String appVersion = conf.getString("app_ver");
-    private static String magicNumber = conf.getString("magic_number");
+    private static final String accessToken = "e9d8b435-0041-4199-8e0c-5cdd80cc9f7b";
 
     public static Result update() {
-        WSRequest request = ws.url(myAdsUrl);
-
-        request.setQueryParameter("api_ver", apiVer);
-        request.setQueryParameter("access_token", accessToken);
-        request.setQueryParameter("session", session);
-        request.setQueryParameter("screen_width", screenWidth);
-        request.setQueryParameter("screen_height", screenHeight);
-        request.setQueryParameter("app_ver", appVersion);
-        request.setQueryParameter("magic_number", magicNumber);
+        Configuration conf = Play.application().configuration();
+        String apiVer = conf.getString("api_ver");
+        String accessToken = conf.getString("access_token");
+        String session = conf.getString("session");
+        String screenWidth = conf.getString("screenWidth");
+        String screenHeight = conf.getString("screenHeight");
+        String appVersion = conf.getString("app_ver");
+        String magicNumber = conf.getString("magic_number");
+        String adId = conf.getString("ad_id");
+//        WSRequest request = ws.url(myAdsUrl);
+//        request.setQueryParameter("", )
+//        return ok(uno.render(20));
         return ok(uno.render(20));
     }
 
